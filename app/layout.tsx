7d4e94f302c8b103a94f2fbb14454e71fb4aa70a
Akash Sahani <akashsahani2205@gmail.com>
2025-05-05 10:27:45 +0530

    Add Ecommet Solutions to metadata and create site.webmanifest for improved PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
   description:
     "Ecommet is your trusted partner for eCommerce, web, and mobile app development. We specialize in Magento, Shopify, custom websites, and scalable digital solutions for startups and enterprises.",
   keywords: [
+    "Ecommet Solutions",
     "Magento Developer",
     "Adobe Certified Magento Developer",
     "Shopify Developer",
@@ -70,6 +71,8 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </head>
       <body className={`${inter.className} bg-[#030014]`}>
         <ThemeProvider
